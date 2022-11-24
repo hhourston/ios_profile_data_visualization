33b0c770ce8694022c69c89ecda4c65357a8288e
Hana Hourston <hana.hourston@gmail.com>
2022-11-23 17:14:04 -0800

    update bill's oxygen datasets and plots

--- 12_plot_o2_on_density_surfaces.py
+++ 12_plot_o2_on_density_surfaces.py
@@ -1,7 +1,71 @@
+import glob
+import gsw
 import pandas as pd
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+
+
+OXY_UMOL_PER_ML = 44.661
+M3_PER_L = 0.001
+
+
+def do_scatter_plot(ax, sigma_theta, station, oxy_data, year_data,
+                    marker_color):
+    return ax.scatter(
+            year_data,
+            oxy_data,
+            label='{} {}'.format(station, np.round(sigma_theta, 1)),
+            marker='o',
+            s=20,
+            edgecolor='k',
+            c=marker_color
+        )
+
+
+def format_scatter_plot(ax, station):
+    ybot, ytop = plt.ylim()
+    ax.set_ylim(top=ytop + 30)  # Give enough space for legend
+    # Major and minor ticks sticking inside and outside the axes
+    # Set one minor tick between each major tick
+    ax.minorticks_on()
+    major_xticks = ax.get_xticks(minor=False)  # in data coords
+    minor_xticks = major_xticks[:-1] + (major_xticks[1] - major_xticks[0]) / 2
+    ax.set_xticks(ticks=minor_xticks, minor=True)
+    major_yticks = ax.get_yticks(minor=False)  # in data coords
+    minor_yticks = major_yticks[:-1] + (major_yticks[1] - major_yticks[0]) / 2
+    ax.set_yticks(ticks=minor_yticks, minor=True)
+    plt.tick_params(which='major', direction='inout')
+    plt.tick_params(which='minor', direction='in')
+    # Send grid lines to back
+    ax.set_axisbelow(True)
+    # Set labels
+    ax.set_ylabel(r'Oxygen [$\mu$mol/kg]')
+    ax.set_title('Annual average oxygen concentration at {}'.format(station))
+    # Add legend with specific properties
+    plt.legend(loc='upper center', ncol=3, edgecolor='k', framealpha=1)
+    plt.tight_layout()
+    return
+
+
+def compute_fit(x, y, fit_degrees):
+    x_values_sorted = np.array(sorted(x))
+    y_values_sorted = np.array([i for _, i in
+                                sorted(zip(x, y))])
+    # Remove any nans otherwise polynomial crashes
+    x_values_sorted = x_values_sorted[~np.isnan(y_values_sorted)]
+    y_values_sorted = y_values_sorted[~np.isnan(y_values_sorted)]
+    # Update polynomial module access from legacy access
+    poly = np.polynomial.Polynomial.fit(
+        x_values_sorted, y_values_sorted, deg=fit_degrees)
+    # coeffs = poly.coef
+    # fit_eqn = np.polynomial.Polynomial(coeffs[::-1]) # must reverse order coeffs
+    # y_hat_sorted = fit_eqn(x_values_sorted)
+    # ax.plot(x_values_sorted, y_hat_sorted, c=c)
+    x_linspace, y_hat_linspace = poly.linspace(n=100)
+
+    # numpy.linalg.LinAlgError: SVD did not converge in Linear Least Squares for OSP
+    return x_linspace, y_hat_linspace
 
 
 def plot_avg_oxy_on_density_surfaces(df_file, png_name, station,
@@ -32,57 +96,65 @@ def plot_avg_oxy_on_density_surfaces(df_file, png_name, station,
         year_data = df.loc[msk, 'Year'].to_numpy(dtype='int32')
         oxy_data = df.loc[msk, 'Average oxygen [umol/kg]'
                           ].to_numpy(dtype='float')
-        ax.scatter(
-            year_data,
-            oxy_data,
-            label='{} {}'.format(station, np.round(sigma_theta, 1)),
-            marker='o',
-            s=20,
-            edgecolor='k',
-            c=c
-        )
+
+        do_scatter_plot(ax, sigma_theta, station, oxy_data, year_data, c)
+
         # Add best-fit line
         if include_fit:
-            x_values_sorted = np.array(sorted(year_data))
-            y_values_sorted = np.array([i for _, i in
-                                        sorted(zip(year_data, oxy_data))])
-            # Remove any nans otherwise polynomial crashes
-            x_values_sorted = x_values_sorted[~np.isnan(y_values_sorted)]
-            y_values_sorted = y_values_sorted[~np.isnan(y_values_sorted)]
-            # Update polynomial module access from legacy access
-            poly = np.polynomial.Polynomial.fit(
-                x_values_sorted, y_values_sorted, deg=fit_deg)
-            # coeffs = poly.coef
-            # fit_eqn = np.polynomial.Polynomial(coeffs[::-1]) # must reverse order coeffs
-            # y_hat_sorted = fit_eqn(x_values_sorted)
-            # ax.plot(x_values_sorted, y_hat_sorted, c=c)
-            x_linspace, y_hat_linspace = poly.linspace(n=100)
+            if len(msk) < 3:
+                print('Warning: not enough points to plot best fit line',
+                      'for {} density surface'.format(sigma_theta))
+                continue
+            x_linspace, y_hat_linspace = compute_fit(year_data, oxy_data,
+                                                     fit_deg)
             ax.plot(x_linspace, y_hat_linspace, c=c)
-            # numpy.linalg.LinAlgError: SVD did not converge in Linear Least Squares for OSP
 
-    ybot, ytop = plt.ylim()
-    ax.set_ylim(top=ytop + 30)  # Give enough space for legend
-    # Major and minor ticks sticking inside and outside the axes
-    # Set one minor tick between each major tick
-    ax.minorticks_on()
-    major_xticks = ax.get_xticks(minor=False)  # in data coords
-    minor_xticks = major_xticks[:-1] + (major_xticks[1] - major_xticks[0])/2
-    ax.set_xticks(ticks=minor_xticks, minor=True)
-    major_yticks = ax.get_yticks(minor=False)  # in data coords
-    minor_yticks = major_yticks[:-1] + (major_yticks[1] - major_yticks[0]) / 2
-    ax.set_yticks(ticks=minor_yticks, minor=True)
-    plt.tick_params(which='major', direction='inout')
-    plt.tick_params(which='minor', direction='in')
-    # Send grid lines to back
-    ax.set_axisbelow(True)
-    # Set labels
-    ax.set_ylabel('Oxygen [umol/kg]')
-    ax.set_title('Annual average oxygen concentration at {}'.format(station))
-    # Add legend with specific properties
-    plt.legend(loc='upper center', ncol=3, edgecolor='k', framealpha=1)
+    format_scatter_plot(ax, station)
 
-    plt.tight_layout()
+    plt.savefig(png_name)
+    plt.close(fig)
+    return
 
+
+def update_bill_oxy_plot(df_file_265, df_file_267, df_file_269, station,
+                         fit_deg, png_name):
+    # Version of the above function but for different df structure
+    fig, ax = plt.subplots()
+    # Add grid lines
+    plt.grid(color='lightgrey')
+
+    for sigma_theta, df_file, c in zip(
+        [26.9, 26.7, 26.5],
+        [df_file_269, df_file_267, df_file_265],
+        ['r', 'y', 'b']
+    ):
+        df = pd.read_csv(df_file)
+
+        # if 'O2 ann avg (umol/kg)' in df.columns:
+        #     o2_avg_colname = 'O2 ann avg (umol/kg)'
+        # elif 'O2 Ann Avg (umol/kg) ' in df.columns:
+        #     o2_avg_colname = 'O2 Ann Avg (umol/kg) '
+        # else:
+        #     print('Oxygen data in umol/kg not found')
+        o2_avg_colname = df.columns[15]
+        print('avg oxy column name:', o2_avg_colname)
+
+        mask_annual = pd.notna(df[o2_avg_colname])
+        o2_avg_data = df.loc[mask_annual, o2_avg_colname]
+        year_data = df.loc[mask_annual, 'Date'].astype(int)
+
+        do_scatter_plot(ax, sigma_theta, station, o2_avg_data, year_data, c)
+
+        # Add best-fit line
+        if len(mask_annual) < 3:
+            print('Warning: not enough points to plot best fit line',
+                      'for {} density surface'.format(sigma_theta))
+            continue
+        x_linspace, y_hat_linspace = compute_fit(year_data, o2_avg_data,
+                                                 fit_deg)
+        ax.plot(x_linspace, y_hat_linspace, c=c)
+
+    format_scatter_plot(ax, station)
     plt.savefig(png_name)
     plt.close(fig)
     return
@@ -92,22 +164,26 @@ def plot_avg_oxy_on_density_surfaces(df_file, png_name, station,
 
 # Plot the oxygen on constant density surfaces
 
-parent_dir = 'C:\\Users\\HourstonH\\Documents\\charles\\' \
-             'line_P_data_products\\csv\\has_osd_ctd_flags\\'
+# parent_dir = 'C:\\Users\\HourstonH\\Documents\\charles\\' \
+#              'line_P_data_products\\csv\\has_osd_ctd_flags\\'
 # parent_dir = 'C:\\Users\\HourstonH\\Documents\\charles\\' \
 #              'bottom_oxygen\\'
+parent_dir = 'D:\\lineP\\csv_data\\'
 
 # for each station: P4 and P26, LB08
 stn = 'P26'
-# station_name = stn
-station_name = 'OSP'
+station_name = 'OSP' if stn == 'P26' else stn
 # print(stn, station_name)
 # data_types = 'CTD_BOT_CHE_OSD'
-best_fit_degrees = 1  # One for P26 and 2 for P4
+best_fit_degrees = 2 if stn == 'P4' else 1  # One for P26 and 2 for P4
 
 average_file = os.path.join(
     parent_dir,
-    '11N_annual_avg_on_dens_surfaces\\{}_data.csv'.format(stn))
+    '11_annual_avg_on_dens_surfaces\\{}_data.csv'.format(stn))
+# average_file = os.path.join(
+#     parent_dir,
+#     '11N_annual_avg_on_dens_surfaces\\{}_ctd_data_qc.csv'.format(stn))
+
 plot_name = average_file.replace(
     '.csv',
     '_oxy_vs_pot_dens_anom_{}degfit.png'.format(best_fit_degrees))
@@ -116,3 +192,59 @@ plot_name = average_file.replace(
 plot_avg_oxy_on_density_surfaces(average_file, plot_name,
                                  station_name, True, best_fit_degrees)
 
+# ---------------------------Bill data---------------------------------
+
+"""
+bill_dir = 'C:\\Users\\HourstonH\\Documents\\charles\\' \
+           'line_P_data_products\\bill_crawford\\masked\\'
+
+p26_file = bill_dir + 'CrawfordPena Line P 1950-2015 26 oxy annual avg.csv'
+
+p4_file = bill_dir + 'CrawfordPena Line P 1950-2015 4849 oxy annual avg.csv'
+
+bill_p26_plot_name = bill_dir + 'CrawfordPena P26 1950-2015 oxy vs sigma-theta.png'
+bill_p4_plot_name = bill_dir + 'CrawfordPena P4 1950-2015 oxy vs sigma-theta.png'
+
+# P26 P4
+station_name = 'P26'
+plot_avg_oxy_on_density_surfaces(p26_file, bill_p26_plot_name,
+                                 station_name, include_fit=True, fit_deg=1)
+
+station_name = 'P4'
+plot_avg_oxy_on_density_surfaces(p4_file, bill_p4_plot_name,
+                                 station_name, include_fit=True, fit_deg=2)
+"""
+
+bill_dir = 'C:\\Users\\HourstonH\\Documents\\charles\\' \
+           'line_P_data_products\\bill_crawford\\'
+
+bill_p4_files = glob.glob(bill_dir + '*4849*_Nov2022.csv')
+bill_p4_files.sort()
+bill_p26_files = glob.glob(bill_dir + '*26_Nov2022.csv')
+bill_p26_files.sort()
+
+indf = pd.read_csv(bill_p4_files[0])
+
+stn = 'P4'
+update_bill_oxy_plot(*bill_p4_files, stn, 2,
+                     bill_dir + f'CrawfordPena_{stn}_1950-2022_o2.png')
+
+stn = 'P26'
+update_bill_oxy_plot(*bill_p26_files, stn, 1,
+                     bill_dir + f'CrawfordPena_{stn}_1955-2022_o2.png')
+
+# # Check if any rows are missing oxygen data
+# print(indf.loc[pd.isna(indf['Ox (umol/kg) '])])
+# # Convert oxy units of these data to umol/kg
+# # Compute density from absolute salinity and conservative temperature
+# mask = pd.isna(indf['Ox (umol/kg) '])
+# pressure = gsw.p_from_z(-indf.loc[mask, 'Depth'].to_numpy(float),
+#                         indf.loc[mask, 'Latitude'].to_numpy(float))
+# density = gsw.rho(indf.loc[mask, 'Absolute Salinity'].to_numpy(float),
+#                   indf.loc[mask, 'Conservative Temperature'].to_numpy(float),
+#                   pressure)
+# indf.loc[mask, 'Ox (umol/kg) '] = [
+#     o * OXY_UMOL_PER_ML / (d * M3_PER_L)
+#     for o, d in zip(indf.loc[mask, 'Ox (ml/L)'].to_numpy(float),
+#                     density)
+# ]
--- lineP/update_bill_lineP_dataset.py
+++ lineP/update_bill_lineP_dataset.py
@@ -3,6 +3,86 @@ import os
 import glob
 import numpy as np
 
+
+def do_update(bill_file_265, bill_file_267, bill_file_269, hana_file,
+              nrows_to_drop=None):
+    input_file_list = [bill_file_265, bill_file_267, bill_file_269]
+    densities = [26.5, 26.7, 26.9]
+
+    hana_df = pd.read_csv(hana_file)
+    for f, d in zip(input_file_list, densities):
+        print(os.path.basename(f))
+        dfin = pd.read_csv(f)
+
+        # if 'Ox (umol/kg) ' in dfin.columns:
+        #     o2_colname = 'Ox (umol/kg) '
+        # elif 'O2 (umol/kg) ' in dfin.columns:
+        #     o2_colname = 'O2 (umol/kg) '
+        # else:
+        #     print('Oxygen data in umol/kg not found')
+
+        o2_colname = dfin.columns[8]
+        o2_avg_colname = dfin.columns[15]
+        print(o2_colname, o2_avg_colname)
+
+        dfin.dropna(axis=0, how='all',
+                    subset=[o2_colname, 'Date',
+                            'Sigma_Theta (from CT and AS)'],
+                    inplace=True)
+        dfin.reset_index(drop=True, inplace=True)
+
+        if nrows_to_drop is not None:
+            # drop the last few rows
+            dfin.drop(dfin.index[-nrows_to_drop:], inplace=True)
+            print('Dropped', nrows_to_drop, 'rows from the dataframe tail')
+            dfin.loc[:, 'Date'] = pd.to_numeric(dfin['Date']).values
+
+        dfin.loc[:, o2_colname] = pd.to_numeric(dfin[o2_colname]).values
+
+        # Append to bottom of dataframe
+        mask = ((hana_df['Year'] > dfin['Date'].max()) &
+                (hana_df['Potential density anomaly bin [kg/m]'] == d))
+        print(sum(mask), 'observations to add')
+
+        new_df_dims = (sum(mask), len(dfin.columns))
+        df_to_add = pd.DataFrame(
+            np.repeat(np.nan, new_df_dims[0] * new_df_dims[1]).reshape(new_df_dims),
+            columns=dfin.columns
+        )
+        df_to_add.loc[:, 'Longitude'] = hana_df.loc[mask, 'Longitude [deg E]'].values
+        df_to_add.loc[:, 'Latitude'] = hana_df.loc[mask, 'Latitude [deg N]'].values
+        df_to_add.loc[:, 'Depth'] = hana_df.loc[mask, 'Depth [m]'].values
+        df_to_add.loc[:, 'Sigma_Theta (from CT and AS)'] = hana_df.loc[
+            mask, 'Potential density anomaly [kg/m]'].values
+        df_to_add.loc[:, o2_colname] = hana_df.loc[mask, 'Oxygen [umol/kg]'].values
+        df_to_add.loc[:, 'Date'] = hana_df.loc[mask, 'Year'].values
+        df_to_add.loc[:, 'File'] = hana_df.loc[mask, 'File'].values
+        if 'Day of Year' in dfin.columns:
+            df_to_add.loc[:, 'Day of Year'] = hana_df.loc[mask, 'Day of year'].values
+
+        dfout = pd.concat((dfin, df_to_add))
+        dfout.reset_index(drop=True, inplace=True)
+
+        # Update or compute the annual averages
+        min_added_year = int(np.nanmin(df_to_add.loc[:, 'Date']))
+        max_added_year = int(np.nanmax(df_to_add.loc[:, 'Date']))
+
+        print('Years to compute new average:', min_added_year, 'to',
+              max_added_year)
+        for y in range(min_added_year, max_added_year + 1):
+            mask = dfout['Date'].astype(int) == y
+            # Find index of first observation in the year
+            dfout.loc[dfout.index[mask][0], o2_avg_colname
+                      ] = dfout.loc[mask, o2_colname].mean()
+            # print('Computed mean for', d)
+
+        # Save the df
+        dfout_filename = f.replace('.csv', '_Nov2022.csv')
+        dfout.to_csv(dfout_filename, index=False)
+        print('df saved to', os.path.basename(dfout_filename))
+    return
+
+
 parent_dir = 'C:\\Users\\HourstonH\\Documents\\charles\\' \
              'line_P_data_products\\bill_crawford\\'
 
@@ -16,79 +96,8 @@ bill_p26_files.sort()
 hana_p4_file = os.path.join(d_drive, 'P4_data.csv')
 hana_p26_file = os.path.join(d_drive, 'P26_data.csv')
 
-hana_p4_df = pd.read_csv(hana_p4_file)
-hana_p26_df = pd.read_csv(hana_p26_file)
+p26_nrows_to_drop = 2
 
-densities = [26.5, 26.7, 26.9]
+do_update(*bill_p4_files, hana_p4_file)
 
-for f, d in zip(bill_p4_files, densities):
-    print(os.path.basename(f))
-    dfin = pd.read_csv(f)
-    dfin.dropna(axis=0, how='all', inplace=True)
-    dfin.reset_index(drop=True, inplace=True)
-    # print(dfin.columns)
-
-    # Append to bottom of dataframe
-    mask = ((hana_p4_df['Year'] > dfin['Date'].max()) &
-            (hana_p4_df['Potential density anomaly bin [kg/m]'] == d))
-    print(sum(mask), 'observations to add')
-
-    new_df_dims = (sum(mask), len(dfin.columns))
-    df_to_add = pd.DataFrame(
-        np.repeat(np.nan, new_df_dims[0] * new_df_dims[1]).reshape(new_df_dims),
-        columns=dfin.columns
-    )
-    df_to_add.loc[:, 'Longitude'] = hana_p4_df.loc[mask, 'Longitude [deg E]'].values
-    df_to_add.loc[:, 'Latitude'] = hana_p4_df.loc[mask, 'Latitude [deg N]'].values
-    df_to_add.loc[:, 'Depth'] = hana_p4_df.loc[mask, 'Depth [m]'].values
-    df_to_add.loc[:, 'Sigma_Theta (from CT and AS)'] = hana_p4_df.loc[
-        mask, 'Potential density anomaly [kg/m]'].values
-    df_to_add.loc[:, 'Ox (umol/kg) '] = hana_p4_df.loc[mask, 'Oxygen [umol/kg]'].values
-    df_to_add.loc[:, 'Date'] = hana_p4_df.loc[mask, 'Year'].values
-    df_to_add.loc[:, 'File'] = hana_p4_df.loc[mask, 'File'].values
-    if 'Day of Year' in dfin.columns:
-        df_to_add.loc[:, 'Day of Year'] = hana_p4_df.loc[mask, 'Day of year'].values
-
-    dfout = pd.concat((dfin, df_to_add))
-
-    # Save the df
-    dfout_filename = f.replace('.csv', '_Nov2022.csv')
-    dfout.to_csv(dfout_filename, index=False)
-
-# repeat for p26
-for f, d in zip(bill_p26_files, densities):
-    print(os.path.basename(f))
-    dfin = pd.read_csv(f)
-    dfin.dropna(axis=0, how='all', inplace=True)
-    dfin.reset_index(drop=True, inplace=True)
-    # drop the last few rows
-    nrows_to_drop = 2
-    dfin.drop(dfin.index[-nrows_to_drop:], inplace=True)
-    # print(dfin.columns)
-
-    # Append to bottom of dataframe
-    mask = ((hana_p26_df['Year'] > dfin['Date'].astype(float).max()) &
-            (hana_p26_df['Potential density anomaly bin [kg/m]'] == d))
-    print(sum(mask), 'observations to add')
-
-    new_df_dims = (sum(mask), len(dfin.columns))
-    df_to_add = pd.DataFrame(
-        np.repeat(np.nan, new_df_dims[0] * new_df_dims[1]).reshape(new_df_dims),
-        columns=dfin.columns
-    )
-    df_to_add.loc[:, 'Longitude'] = hana_p26_df.loc[mask, 'Longitude [deg E]'].values
-    df_to_add.loc[:, 'Latitude'] = hana_p26_df.loc[mask, 'Latitude [deg N]'].values
-    df_to_add.loc[:, 'Depth'] = hana_p26_df.loc[mask, 'Depth [m]'].values
-    df_to_add.loc[:, 'Sigma_Theta (from CT and AS)'] = hana_p26_df.loc[
-        mask, 'Potential density anomaly [kg/m]'].values
-    df_to_add.loc[:, 'Ox (umol/kg) '] = hana_p26_df.loc[mask, 'Oxygen [umol/kg]'].values
-    df_to_add.loc[:, 'Date'] = hana_p26_df.loc[mask, 'Year'].values
-    df_to_add.loc[:, 'File'] = hana_p26_df.loc[mask, 'File'].values
-    if 'Day of Year' in dfin.columns:
-        df_to_add.loc[:, 'Day of Year'] = hana_p26_df.loc[mask, 'Day of year'].values
-
-    dfout = pd.concat((dfin, df_to_add))
-
-    # Save the df
-    dfout_filename = f.replace('.csv', '_Nov2022.csv')
-    dfout.to_csv(dfout_filename, index=False)
+do_update(*bill_p26_files, hana_p26_file, p26_nrows_to_drop)
