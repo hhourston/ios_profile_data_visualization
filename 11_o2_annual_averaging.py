76e406118b5807f2f3ad5c3ceb985631d1b3ca96
hhourston <hana.hourston@dfo-mpo.gc.ca>
2024-01-19 17:11:31 -0800

    amend annual averaging function to accept a concatenated dataframe instead of a single dataframe filename

--- 11_o2_annual_averaging.py
+++ 11_o2_annual_averaging.py
@@ -3,11 +3,11 @@ import numpy as np
 import os
 
 
-def annual_avg_on_density_surfaces(input_file_name: str, output_file_name: str,
-                                   density_surfaces: list, station: str):
+def annual_avg_on_density_surfaces(input_df: pd.DataFrame, output_file_name: str,
+                                   density_surfaces: list, station: str, input_file_names: str):
     # Take annual average of oxygen data on potential density anomaly surfaces
 
-    df = pd.read_csv(input_file_name)
+    df = input_df
     # df columns are: Profile number, Time, Profile is interpolated,
     # Depth [m]	Oxygen [umol/kg], Potential density anomaly [kg/m]
     # Potential density anomaly bin [kg/m]
@@ -20,9 +20,7 @@ def annual_avg_on_density_surfaces(input_file_name: str, output_file_name: str,
 
     # Initialize dataframe to hold annual averages
     density_column = 'Potential density anomaly bin [kg/m]'
-    df_avg = pd.DataFrame(
-        columns=['Year', density_column,
-                 'Average oxygen [umol/kg]'])
+    df_avg = pd.DataFrame(columns=['Year', density_column, 'Average oxygen [umol/kg]'])
 
     # Take the average for each year and density level
     for i in range(len(years_available)):
@@ -34,11 +32,9 @@ def annual_avg_on_density_surfaces(input_file_name: str, output_file_name: str,
             # Append a new row to the end of the initialized df
             if len(indexer) > 0:
                 df_avg.loc[len(df_avg)] = [
-                    years_available[i], density_surfaces[j], np.nanmean(
-                        df.loc[indexer,
-                               'Oxygen [umol/kg]'].to_numpy(
-                            float)
-                    )
+                    years_available[i],
+                    density_surfaces[j],
+                    np.nanmean(df.loc[indexer, 'Oxygen [umol/kg]'].to_numpy(float))
                 ]
             # Update summary statistics
             obs_per_year[i, j] = len(indexer)
@@ -48,7 +44,7 @@ def annual_avg_on_density_surfaces(input_file_name: str, output_file_name: str,
         os.path.dirname(output_file_name),
         '{}_averaging_summary_statistics.txt'.format(station))
     with open(summary_file, 'w') as txtfile:
-        txtfile.write('Input file: ' + input_file_name + '\n')
+        txtfile.write('Input file: ' + input_file_names + '\n')
         txtfile.write('Output file: ' + output_file_name + '\n')
         txtfile.write('Number of observations:\n')
         txtfile.write('Year')
@@ -64,7 +60,7 @@ def annual_avg_on_density_surfaces(input_file_name: str, output_file_name: str,
 
 
 # for each station: P4 and P26, LB08
-stn = 'P26'
+stn = 'P4'
 # station_name = stn
 # station_name = 'OSP'
 
@@ -73,22 +69,30 @@ stn = 'P26'
 # parent_dir = 'C:\\Users\\HourstonH\\Documents\\charles\\' \
 #              'bottom_oxygen\\'
 
-parent_dir = 'D:\\lineP\\csv_data\\'
+# parent_dir = 'D:\\lineP\\processing\\'
+
+parent_dir = ('C:\\Users\\hourstonh\\Documents\\charles\\line_P_data_products\\'
+              'update_jan2024_sopo\\csv_data\\')
 
 o2_bin_dir = '10_bin_o2_to_select_densities'
-o2_bin_file = os.path.join(parent_dir, o2_bin_dir,
-                           '{}_data.csv'.format(stn))
+o2_bin_file = os.path.join(parent_dir, o2_bin_dir, '{}_data.csv'.format(stn))
 # o2_bin_file = os.path.join(parent_dir, o2_bin_dir,
 #                            '{}_ctd_data_qc.csv'.format(stn))
 
 avg_dir = '11_annual_avg_on_dens_surfaces'
-avg_file = os.path.join(parent_dir, avg_dir,
-                        os.path.basename(o2_bin_file))
+avg_file = os.path.join(parent_dir, avg_dir, os.path.basename(o2_bin_file))
 
 densities = [26.5, 26.7, 26.9]
 
-annual_avg_on_density_surfaces(o2_bin_file, avg_file,
-                               densities, stn)
+# Pre-2022 data
+pre_2022_file = f'D:\\lineP\\processing\\10_bin_o2_to_select_densities\\{stn}_data.csv'
+
+df_merged = pd.read_csv(o2_bin_file)
+df_merged = pd.concat((pd.read_csv(pre_2022_file), df_merged))
+df_merged.reset_index(drop=True, inplace=True)
+
+annual_avg_on_density_surfaces(df_merged, avg_file, densities, stn,
+                               input_file_names=' & '.join([o2_bin_file, pre_2022_file]))
 
 # ------------------------Bill's data------------------------
 """
